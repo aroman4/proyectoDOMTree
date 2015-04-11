83e92ac7626028e660e954e952fe6868deded03d
aroman4 <aroman4@users.noreply.github.com>
2015-04-10 22:45:23 -0430

    Update Element.h

--- Element.h
+++ Element.h
@@ -1,6 +1,8 @@
 #ifndef Element_H
 #define Element_H
 
+#include <list>
+
 using namespace std;
 
 class Element{
@@ -8,6 +10,7 @@ class Element{
 		//Atributos
 		string tagNam;
 		list<string> attrList;
+		string Id;
 		string innerHtml;
 		
 	public:
@@ -16,16 +19,20 @@ class Element{
 		Element (string tag) : tagNam(tag) {}
 		Element (string tag,list<string> attr) : tagNam(tag), attrList(attr) {}
 		Element (string tag,list<string> attr,string html) : tagNam(tag), attrList(attr), innerHtml(html) {}
-		Element (const Element& E) {tagNam = E.tagNam; attrList = E.attrList; innerHtml = E.innerHtml;}
+		Element (const Element& E) {tagNam = E.tagNam; attrList = E.attrList; innerHtml = E.innerHtml; Id = E.Id;}
+		Element& operator=(const Element& E){if(this != &E){this->tagNam = E.tagNam; this->attrList = E.attrList; this->innerHtml = E.innerHtml; this->Id = E.Id;}return *this;}
 		//Gets
 		string tagName(){return tagNam;}
 		list<string> attributeList () {return attrList;}
 		string innerHTML () {return innerHtml;}
+		string ID () {return Id;}
 		//Sets
 		void setTagName (string tag) {tagNam = tag;}
 		void setAttributeList (list<string> L) {attrList = L;}
 		void setInnerHTML (string html) {innerHtml = html;}
+		void setID (string id) {Id = id;}
 	
 };
 
 #endif
+
